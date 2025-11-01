d3d9c0e7de7640068c4a23f1ca3525dc8109275d
FaizAhmad0 <118246870+FaizAhmad0@users.noreply.github.com>
2025-11-01 11:51:48 +0530

    initial for deploying

--- client/src/pages/adminPages/AdminDash.jsx
+++ client/src/pages/adminPages/AdminDash.jsx
@@ -22,7 +22,7 @@ const AdminDash = () => {
     <AdminLayout>
       <div className="p-6">
         <h2 className="text-2xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-red-500 text-transparent bg-clip-text inline-block max-w-full whitespace-normal">
-          Actions Needing Your Attention Today for Self-ship Orders
+          Actions Needing Your Attention Today for Orders
         </h2>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -34,7 +34,7 @@ const AdminDash = () => {
           <EasyShipArchive />
         </div>
         <h2 className="text-2xl mt-16 font-bold mb-6 bg-gradient-to-r from-blue-500 to-red-500 text-transparent bg-clip-text inline-block max-w-full whitespace-normal">
-          Summary of Self-ship Orders
+          Summary of Orders
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"></div>
         <EasyshipRevenu />
