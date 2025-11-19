9de2eddcfc71aa8546f3c0f467f2842a944f51ef
FaizAhmad0 <118246870+FaizAhmad0@users.noreply.github.com>
2025-11-19 18:02:04 +0530

    allowed manager for search the user

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ app.post("/callbackurl", (req, res) => {
         "Would redirect to success page with:",
         queryParams.toString()
       );
-      return res.redirect(`https://wallet.saumiccraft.com/payment-status`);
+      return res.redirect(`https://wallet.saumiccraft.com/wallet`);
       // return res.json({ ok: true, payload: decoded, redirectUrl: `http://localhost:8080/#/payment-success?${queryParams.toString()}` });
     } else {
       const reason =
